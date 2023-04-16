6c193d52a92f8161af6b7b3873b8406836187279
Sachin Yo <sachin_aryal>
2023-04-16 11:00:46 +0545

    Fixed UI issue on custom team select item component

--- src/components/team/team-member-select-item/team-member-select-item.tsx
+++ src/components/team/team-member-select-item/team-member-select-item.tsx
@@ -1,4 +1,4 @@
-import { Avatar, Badge, Box, BoxProps, Group, Text } from '@mantine/core'
+import { Avatar, Badge, Box, BoxProps, Flex, Group, Text } from '@mantine/core'
 import { forwardRef } from 'react'
 
 export interface TeamMemberSelectItemProps extends BoxProps {
@@ -13,18 +13,20 @@ export const TeamMemberSelectItem = forwardRef<
 >(({ label, description, image, isAlreadyOnATeam, ...rest }, ref) => {
   return (
     <Box ref={ref} {...rest}>
-      <Group align='flex-start' noWrap>
-        <Avatar radius='xl' src={image ?? '/images/default_user.png'} />
-        <Box sx={{ flex: 1 }}>
-          <Text>{label}</Text>
-          <Text color='dimmed' size='xs'>
-            {description}
-          </Text>
-        </Box>
+      <Flex align='flex-start' justify='space-between' wrap='nowrap'>
+        <Group>
+          <Avatar radius='xl' src={image ?? '/images/default_user.png'} />
+          <Box>
+            <Text>{label}</Text>
+            <Text color='dimmed' size='xs'>
+              {description}
+            </Text>
+          </Box>
+        </Group>
         <Badge color={isAlreadyOnATeam ? 'red' : 'green'} fw='bold'>
           {isAlreadyOnATeam ? 'Not Available' : 'Available'}
         </Badge>
-      </Group>
+      </Flex>
     </Box>
   )
 })
